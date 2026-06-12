e9c61480c92fb97259179e2a053f866335705223
Fatin Ishraq Prapya <dev.prapya@gmail.com>
2026-06-11 20:31:26 -0700

    basic better auth setup done

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,7 @@
 import express, { Router } from "express";
 import adminRouter from "../modules/admin/admin.routes.js";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "../config/auth.js";
 
 interface AppRoute {
     path: string;
@@ -9,7 +11,8 @@ interface AppRoute {
 const router = express.Router();
 
 const routes: AppRoute[] = [
-    { path: "/admin", handler: adminRouter }
+    { path: "/admin", handler: adminRouter },
+    { path: "/auth/*", handler: toNodeHandler(auth) as any }
 ];
 
 routes.forEach(route => {
